7afb2666e7898a7c1cf0d8810e2b9d0baf07a60d
wangte <wangte@startdt.com>
2018-11-08 18:20:59 +0000

    init copy from the tutorial

---
+++ src/Routes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+
+export default ({childProps}) =>
+    <Switch>
+        <AppliedRoute path="/" exact component={Home} props={childProps} />
+        <AppliedRoute path="/login" exact component={Login} props={childProps}/>
+        <AppliedRoute path={"/signup"} exact component={Signup} props={childProps}/>
+
+
+        <Route component={NotFound}/>
+    </Switch>;
---
+++ src/containers/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <div className="lander">
+                    <h1>ProjectManagement</h1>
+                </div>
+            </div>
+        );
+    }
+}
